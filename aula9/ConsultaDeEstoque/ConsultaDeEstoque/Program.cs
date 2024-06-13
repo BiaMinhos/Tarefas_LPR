780f9d4f3c9b82524508a5f9bfa733ef07b789bc
beatriz-minhos <138534948+BiaMinhos@users.noreply.github.com>
2024-06-13 15:36:32 -0300

    exercicios

---
+++ aula9/ConsultaDeEstoque/ConsultaDeEstoque/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultaDeEstoque
+{
+    class Program
+    {
+       struct Produto
+        {
+            string Nome;
+            int Codigo;
+            double Preco;
+            int Quantidade;
+        }
+        static void Main(string[] args)
+        {
+            Produto[] loja = new Produto[3];
+            loja[0]= new Produto { Nome=}
+        }
+    }
+}
+
+/*Exercício 1) RESOLVA EM C#
+Defina uma struct Produto que contenha
+os seguintes campos: Nome, Codigo,
+Preco e Quantidade. Crie um programa
+que permita ao usuário inserir dados de 3
+produtos e, em seguida, exiba o valor
+total em estoque (considerando o preço e
+a quantidade de cada produto).*/
